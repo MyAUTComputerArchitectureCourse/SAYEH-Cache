9cb2535ec7ed24ee0f1b4f71391136312bbbb9bf
Ahmad Anvari <ahmad.anv@gmail.com>
2017-07-09 16:11:27 +0430

    Change name of address to index

--- src/data_array.vhd
+++ src/data_array.vhd
@@ -4,12 +4,12 @@ use IEEE.numeric_std.all;
 
 entity DATA_ARRAY is
 	generic(
-		ADDRESS_SIZE : integer;
+		INDEX_SIZE : integer;
 		DATA_SIZE : integer
 	);
 	port(
 		CLK			:	in	std_logic;
-		ADDRESS		:	in	std_logic_vector(ADDRESS_SIZE - 1 downto 0);
+		INDEX		:	in	std_logic_vector(INDEX_SIZE - 1 downto 0);
 		WRITE_EN	:	in	std_logic;
 		IN_DATA		:	in	std_logic_vector(DATA_SIZE - 1 downto 0);
 		OUT_DATA	:	out std_logic_vector(DATA_SIZE - 1 downto 0)
@@ -17,7 +17,7 @@ entity DATA_ARRAY is
 end entity;
 
 architecture DATA_ARRAY_ARCH of DATA_ARRAY is
-	type data_type is array ((2 ** ADDRESS_SIZE) - 1 downto 0) of std_logic_vector (DATA_SIZE - 1 downto 0);
+	type data_type is array ((2 ** INDEX_SIZE) - 1 downto 0) of std_logic_vector (DATA_SIZE - 1 downto 0);
 	
 	signal DATA : data_type;
 	
@@ -26,9 +26,9 @@ begin
 	begin
 		if rising_edge(CLK) then
 			if WRITE_EN = '1' then
-				DATA(to_integer(unsigned(ADDRESS))) <= IN_DATA;
+				DATA(to_integer(unsigned(INDEX))) <= IN_DATA;
 			end if;
-			OUT_DATA <= DATA(to_integer(unsigned(ADDRESS)));
+			OUT_DATA <= DATA(to_integer(unsigned(INDEX)));
 		end if;
 	end process CLK_PRC;
 	
