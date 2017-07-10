94d3f65f9a5689db71b586585e724c46f1885d4c
Ahmad Anvari <ahmad.anv@gmail.com>
2017-07-10 12:32:06 +0430

    Implementing counter_array

--- src/counter_array.vhd
+++ src/counter_array.vhd
@@ -9,13 +9,13 @@ entity COUNTER_ARRAY is
 	);
 	port(
 		CLK			: in  std_logic;
-		INDEX		: in  std_logic_vector(2 ** INDEX_SIZE - 1 downto 0);
-		PLUS_ONE_O	: in  std_logic;
+		INDEX		: in  std_logic_vector(INDEX_SIZE - 1 downto 0);
+		PLUS_ONE_0	: in  std_logic;
 		PLUS_ONE_1	: in  std_logic;
 		RESET_O		: in  std_logic;
 		RESET_1		: in  std_logic;
-		COUNT_OUT_0	: out std_logic_vector(2 ** COUNTER_SIZE - 1 downto 0);
-		COUNT_OUT_1	: out std_logic_vector(2 ** COUNTER_SIZE - 1 downto 0)
+		COUNT_OUT_0	: out std_logic_vector(COUNTER_SIZE - 1 downto 0);
+		COUNT_OUT_1	: out std_logic_vector(COUNTER_SIZE - 1 downto 0)
 	);
 end entity;
 
@@ -24,7 +24,7 @@ architecture COUNTER_ARRAY_ARCH of COUNTER_ARRAY is
 	signal COUNTER_0	: counter_arr := (others => (others => '0'));
 	signal COUNTER_1	: counter_arr := (others => (others => '0'));
 begin
-	COUNT : process (CLK) is
+	COUNT : process (CLK, RESET_O, RESET_1, PLUS_ONE_0, PLUS_ONE_1) is
 	begin
 		if rising_edge(CLK) then
 			if RESET_O = '1' then
@@ -33,15 +33,15 @@ begin
 			if RESET_1 = '1' then
 				COUNTER_1(to_integer(unsigned(INDEX))) <= (others => '0');
 			end if;
-			if PLUS_ONE_1 = '1' then
-				COUNTER_0(to_integer(unsigned(INDEX))) <= COUNTER_0(to_integer(unsigned(INDEX))) + 1;
+			if PLUS_ONE_0 = '1' then
+				COUNTER_0(to_integer(unsigned(INDEX))) <= std_logic_vector(unsigned(COUNTER_0(to_integer(unsigned(INDEX)))) + 1);
 			end if;
-			if RESET_1 = '1' then
-				COUNTER_1(to_integer(unsigned(INDEX))) <= COUNTER_1(to_integer(unsigned(INDEX))) + 1;
+			if PLUS_ONE_1 = '1' then
+				COUNTER_1(to_integer(unsigned(INDEX))) <= std_logic_vector(unsigned(COUNTER_1(to_integer(unsigned(INDEX)))) + 1);
 			end if;
 		end if;
 	end process COUNT;
 	
-	COUNT_OUT_0 <= COUNTER_0;
-	COUNT_OUT_1 <= COUNTER_1;
-end architecture DATA_ARRAY_ARCH;
+	COUNT_OUT_0 <= COUNTER_0(to_integer(unsigned(INDEX)));
+	COUNT_OUT_1 <= COUNTER_1(to_integer(unsigned(INDEX)));
+end architecture COUNTER_ARRAY_ARCH;
